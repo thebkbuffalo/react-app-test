19a7cf2ee13e4aba6cce8f602023b0abe173ca43
Evan Berg <evan.m.berg@gmail.com>
2022-04-06 18:18:42 -0400

    getting form rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import TodoListsContainer from "./components/TodoListsContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="mainContainer">
+        <div className="topHeading">
+          <h1>A Simple To-Do List App</h1>
+        </div>
+        <TodoListsContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
