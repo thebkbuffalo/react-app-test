287a5b8663dc826ae0094d993e9ed6ec84003756
Evan Berg <evan.m.berg@gmail.com>
2022-04-21 17:27:01 -0400

    all things working, CRUD and visual cues for marking as done

--- src/components/TodoListsContainer.js
+++ src/components/TodoListsContainer.js
@@ -16,12 +16,42 @@ class TodoListsContainer extends Component {
     componentDidMount(){
         this.loadTodoLists();
     }
+    newListItem = (e) => {
+        var newTitle = document.getElementById('newTaskTitle').value;
+        var newDescription = document.getElementById('newTaskDescription').value;
+        var todo_list = {title: newTitle, description: newDescription, done: false}
+        axios.post('http://localhost:3000/v1/todo_lists', {todo_list}).then((res) => {
+            const newTodoLists = this.state.todoLists.slice();
+            this.setState({todoLists: [res.data, ...newTodoLists]})
+            document.getElementById('newTaskTitle').value = ''
+            document.getElementById('newTaskDescription').value = ''
+        }).catch((error) => console.log(error));
+    }
+    updateDoneStatus = (e, id) => {
+        var isDone = e.currentTarget.checked;
+        var payload = {isDone: isDone}
+        axios.put(`http://localhost:3000/v1/todo_lists/${id}`, {payload}).then((res) => {
+            const newTodoLists = this.state.todoLists.slice();
+            const tdListIndex = this.state.todoLists.findIndex((x) => x.id === res.data.id);
+            newTodoLists[tdListIndex] = res.data;
+            this.setState({todoLists: newTodoLists});
+        });
+    }
+    deleteItem = (e, id) => {
+        axios.delete(`http://localhost:3000/v1/todo_lists/${id}`).then((res) => {
+            const newTodoLists = this.state.todoLists.slice();
+            const tdListIndex = this.state.todoLists.findIndex((x) => x.id === res.data.id);
+            newTodoLists.splice(tdListIndex, 1);
+            this.setState({todoLists: newTodoLists});
+        });
+    }
     render() {
         return (
             <div>
                 <div className='taskContainer'>
                     <input
                         className='taskContainer'
+                        id='newTaskTitle'
                         type='text'
                         placeholder='Task Title'
                         maxLength='125'
@@ -30,20 +60,26 @@ class TodoListsContainer extends Component {
                 <div className='taskContainer'>
                     <input
                         className='taskContainer'
+                        id='newTaskDescription'
                         type='text'
                         placeholder='Description'
                     />
                 </div>
-                <input type='submit' value='Save'/>
+                <input type='submit' value='Save' onClick={this.newListItem}/>
                 <div className='wrapItems'>
                     <ul className='listItems'>
                         {this.state.todoLists.map((tditem) => {
                             return (
                                 <li className='item' tditem={tditem} key={tditem.id}>
-                                    <input className='itemCheckBox' type='checkbox'/>
-                                    <label className='itemDisplay'>  {tditem.title} - </label>
-                                    <span className='itemDescription'>{tditem.description} </span>
-                                    <button className='removeItemBtn'>remove</button>
+                                    <input 
+                                        className='itemCheckBox' 
+                                        type='checkbox' id={tditem.id}
+                                        checked={tditem.done}
+                                        onChange={(e) => this.updateDoneStatus(e, tditem.id)}
+                                    />
+                                    <label className={'itemDisplay'+(tditem.done ? ' strike' : '')}>  {tditem.title} - </label>
+                                    <span className={'itemDescription'+(tditem.done ? ' strike' : '')}>{tditem.description} </span>
+                                    <button className='removeItemBtn' onClick={(e) => this.deleteItem(e, tditem.id)}>remove</button>
                                 </li>
                             );
                         })}
